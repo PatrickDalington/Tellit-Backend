78846554f7b35c473a0f6d4b343bb77c15d59e9d
PatrickDalington <patrickdalington11@gmail.com>
2024-08-16 18:09:13 +0100

    (fix) (Removed Seeding) I remove the seeding entities as they are not needed anymore

--- src/database/seeding/seeding.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SeedingService } from './seeding.service';
-import { SeedingController } from './seeding.controller';
-
-@Module({
-  controllers: [SeedingController],
-  providers: [SeedingService],
-})
-export class SeedingModule {}
